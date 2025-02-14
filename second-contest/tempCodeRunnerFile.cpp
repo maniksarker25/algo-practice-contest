35e35bf543d8027d19c01e2873e64e925bcbf092
manik <maniksarker652@gmail.com>
2025-02-14 23:56:08 +0600

    complete second contest

--- second-contest/tempCodeRunnerFile.cpp
+++ second-contest/tempCodeRunnerFile.cpp
@@ -1,3 +1,4 @@
-    // for (int i = 0; i < n; i++)
-    //     for (int j = 0; j < m; j++)
-    //         cin >> grid[i][j];
+   // pair<int, int> par = parent[x][y];
+        // x = par.first;
+        // y = par.second;
+        // grid[x][y] = 'X';
