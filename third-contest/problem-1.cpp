0f34514f3910a927c4037f114387b19c7b4b7767
manik <maniksarker652@gmail.com>
2025-02-28 17:03:27 +0600

    's'

---
+++ third-contest/problem-1.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+
+int n, e;
+vector<Edge> edge_list;
+int dis[1005];
+bool cycle;
+
+void bellman_ford(int src)
+{
+    dis[src] = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto ed : edge_list)
+        {
+            int a = ed.a, b = ed.b, w = ed.w;
+            if (dis[a] != INT_MAX && dis[a] + w < dis[b])
+            {
+                dis[b] = dis[a] + w;
+            }
+        }
+    }
+
+    for (auto ed : edge_list)
+    {
+        int a = ed.a, b = ed.b, w = ed.w;
+        if (dis[a] != INT_MAX && dis[a] + w < dis[b])
+        {
+            cout << "Negative Cycle Detected" << endl;
+            cycle = true;
+            return;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> e;
+    for (int i = 1; i <= n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        edge_list.push_back(Edge(a, b, w));
+    }
+
+    int src;
+    cin >> src;
+
+    cycle = false;
+    bellman_ford(src);
+
+    if (!cycle)
+    {
+        int t;
+        cin >> t;
+        while (t--)
+        {
+            int d;
+            cin >> d;
+
+            if (dis[d] == INT_MAX)
+            {
+                cout << "Not Possible" << endl;
+            }
+            else
+            {
+                cout << dis[d] << endl;
+            }
+        }
+    }
+
+    return 0;
+}
